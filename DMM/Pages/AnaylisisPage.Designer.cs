0558fd79aa3f30b0e28d8afe7175125e5a9afd66
Dr. Mohammed Qasim <87497268+MohammedTsmu@users.noreply.github.com>
2024-12-11 10:29:51 +0300

    Chart Finished

--- DMM/Pages/AnaylisisPage.Designer.cs
+++ DMM/Pages/AnaylisisPage.Designer.cs
@@ -29,22 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.txt_totoaldebit = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txt_suppleir = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.txt_customer = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_suppleir = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_totoaldebit = new System.Windows.Forms.Label();
             this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
             this.flowLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -53,6 +60,7 @@
             this.flowLayoutPanel1.Controls.Add(this.panel3);
             this.flowLayoutPanel1.Controls.Add(this.panel2);
             this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.chartControl1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -60,80 +68,6 @@
             this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.flowLayoutPanel1.Size = new System.Drawing.Size(1080, 720);
             this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txt_totoaldebit);
-            this.panel1.Location = new System.Drawing.Point(503, 662);
-            this.panel1.Margin = new System.Windows.Forms.Padding(15);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(511, 286);
-            this.panel1.TabIndex = 0;
-            // 
-            // txt_totoaldebit
-            // 
-            this.txt_totoaldebit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
-            this.txt_totoaldebit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.txt_totoaldebit.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_totoaldebit.ForeColor = System.Drawing.Color.White;
-            this.txt_totoaldebit.Location = new System.Drawing.Point(0, 219);
-            this.txt_totoaldebit.Name = "txt_totoaldebit";
-            this.txt_totoaldebit.Size = new System.Drawing.Size(511, 67);
-            this.txt_totoaldebit.TabIndex = 0;
-            this.txt_totoaldebit.Text = "1000000";
-            this.txt_totoaldebit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
-            this.label2.Location = new System.Drawing.Point(3, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(505, 132);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "مجموع ديون العملاء والموردين";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Control;
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.txt_suppleir);
-            this.panel2.Location = new System.Drawing.Point(503, 346);
-            this.panel2.Margin = new System.Windows.Forms.Padding(15);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(511, 286);
-            this.panel2.TabIndex = 0;
-            // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.DarkRed;
-            this.label3.Location = new System.Drawing.Point(71, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(368, 67);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "ديون الموردين";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // txt_suppleir
-            // 
-            this.txt_suppleir.BackColor = System.Drawing.Color.DarkRed;
-            this.txt_suppleir.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.txt_suppleir.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_suppleir.ForeColor = System.Drawing.Color.White;
-            this.txt_suppleir.Location = new System.Drawing.Point(0, 219);
-            this.txt_suppleir.Name = "txt_suppleir";
-            this.txt_suppleir.Size = new System.Drawing.Size(511, 67);
-            this.txt_suppleir.TabIndex = 0;
-            this.txt_suppleir.Text = "1000000";
-            this.txt_suppleir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel3
             // 
@@ -172,6 +106,113 @@
             this.txt_customer.Text = "1000000";
             this.txt_customer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txt_suppleir);
+            this.panel2.Location = new System.Drawing.Point(503, 346);
+            this.panel2.Margin = new System.Windows.Forms.Padding(15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(511, 286);
+            this.panel2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(71, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(368, 67);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ديون الموردين";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_suppleir
+            // 
+            this.txt_suppleir.BackColor = System.Drawing.Color.DarkRed;
+            this.txt_suppleir.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txt_suppleir.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suppleir.ForeColor = System.Drawing.Color.White;
+            this.txt_suppleir.Location = new System.Drawing.Point(0, 219);
+            this.txt_suppleir.Name = "txt_suppleir";
+            this.txt_suppleir.Size = new System.Drawing.Size(511, 67);
+            this.txt_suppleir.TabIndex = 0;
+            this.txt_suppleir.Text = "1000000";
+            this.txt_suppleir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txt_totoaldebit);
+            this.panel1.Location = new System.Drawing.Point(503, 662);
+            this.panel1.Margin = new System.Windows.Forms.Padding(15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(511, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.label2.Location = new System.Drawing.Point(3, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(505, 132);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "مجموع ديون العملاء والموردين";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_totoaldebit
+            // 
+            this.txt_totoaldebit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txt_totoaldebit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txt_totoaldebit.Font = new System.Drawing.Font("LBC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_totoaldebit.ForeColor = System.Drawing.Color.White;
+            this.txt_totoaldebit.Location = new System.Drawing.Point(0, 219);
+            this.txt_totoaldebit.Name = "txt_totoaldebit";
+            this.txt_totoaldebit.Size = new System.Drawing.Size(511, 67);
+            this.txt_totoaldebit.TabIndex = 0;
+            this.txt_totoaldebit.Text = "1000000";
+            this.txt_totoaldebit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.AppearanceNameSerializable = "Northern Lights";
+            this.chartControl1.DataSource = typeof(DMM.Debit_Customer);
+            xyDiagram1.AxisX.DateTimeScaleOptions.AggregateFunction = DevExpress.XtraCharts.AggregateFunction.Sum;
+            xyDiagram1.AxisX.GridLines.MinorVisible = true;
+            xyDiagram1.AxisX.GridLines.Visible = true;
+            xyDiagram1.AxisX.Title.Text = "التاريخ / يوم";
+            xyDiagram1.AxisX.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.Title.Text = "الديون";
+            xyDiagram1.AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Location = new System.Drawing.Point(340, 966);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.PaletteName = "Office";
+            series1.ArgumentDataMember = "DateT";
+            series1.Name = "ديون العملاء";
+            series1.SeriesID = 5;
+            series1.ValueDataMembersSerializable = "Debit";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.SeriesTemplate.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.Size = new System.Drawing.Size(686, 377);
+            this.chartControl1.TabIndex = 1;
+            chartTitle1.DXFont = new DevExpress.Drawing.DXFont("LBC", 18F, DevExpress.Drawing.DXFontStyle.Bold);
+            chartTitle1.Text = "ديون العملاء";
+            chartTitle1.TitleID = 0;
+            this.chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            chartTitle1});
+            // 
             // AnaylisisPage
             // 
             this.Appearance.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -179,14 +220,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "AnaylisisPage";
             this.Size = new System.Drawing.Size(1080, 720);
             this.flowLayoutPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -204,5 +248,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label txt_customer;
         private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
     }
 }
